372279fb38ce339829686624bae22f9d71dc56e6
Thomas Lee <thomaslee@yam.ai>
2019-03-27 22:54:18 +0800

    Change logo

--- conf.py
+++ conf.py
@@ -77,7 +77,7 @@ pygments_style = None
 #
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = ['_themes', ]
-html_logo = "_static/images/yamlogo.svg"
+html_logo = "_static/images/yamlogo.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
